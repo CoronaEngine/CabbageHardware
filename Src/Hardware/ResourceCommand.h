c77b6c8c57d67a3905be6433321a902d1bd25a25
GraphZou <906520989@qq.com>
2025-11-04 09:13:29 +0800

    fix

--- Src/Hardware/ResourceCommand.h
+++ Src/Hardware/ResourceCommand.h
@@ -20,6 +20,45 @@ struct CopyBufferCommand : public CommandRecord
     {
         hardwareExecutor.hardwareContext->resourceManager.copyBuffer(hardwareExecutor.currentRecordQueue->commandBuffer, srcBuffer, dstBuffer);
     }
+
+    CommandRecord::RequiredBarriers getRequiredBarriers(HardwareExecutor& hardwareExecutor)
+    {
+        CommandRecord::RequiredBarriers requiredBarriers;
+        {
+            VkBufferMemoryBarrier2 srcBufferBarrier{};
+            srcBufferBarrier.sType = VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER_2;
+            srcBufferBarrier.srcStageMask = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT;
+            srcBufferBarrier.srcAccessMask = VK_ACCESS_2_MEMORY_WRITE_BIT;
+            srcBufferBarrier.dstStageMask = VK_PIPELINE_STAGE_2_TRANSFER_BIT;
+            srcBufferBarrier.dstAccessMask = VK_ACCESS_2_TRANSFER_READ_BIT;
+            srcBufferBarrier.srcQueueFamilyIndex = hardwareExecutor.currentRecordQueue->queueFamilyIndex;
+            srcBufferBarrier.dstQueueFamilyIndex = hardwareExecutor.currentRecordQueue->queueFamilyIndex;
+            srcBufferBarrier.buffer = srcBuffer.bufferHandle;
+            srcBufferBarrier.offset = srcBuffer.bufferAllocInfo.offset;
+            srcBufferBarrier.size = srcBuffer.bufferAllocInfo.size;
+            srcBufferBarrier.pNext = nullptr;
+
+            requiredBarriers.bufferBarriers.push_back(srcBufferBarrier);
+        }
+        {
+            VkBufferMemoryBarrier2 dstBufferBarrier{};
+
+            dstBufferBarrier.sType = VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER_2;
+            dstBufferBarrier.srcStageMask = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT;
+            dstBufferBarrier.srcAccessMask = VK_ACCESS_2_MEMORY_WRITE_BIT;
+            dstBufferBarrier.dstStageMask = VK_PIPELINE_STAGE_2_TRANSFER_BIT;
+            dstBufferBarrier.dstAccessMask = VK_ACCESS_2_TRANSFER_WRITE_BIT;
+            dstBufferBarrier.srcQueueFamilyIndex = hardwareExecutor.currentRecordQueue->queueFamilyIndex;
+            dstBufferBarrier.dstQueueFamilyIndex = hardwareExecutor.currentRecordQueue->queueFamilyIndex;
+            dstBufferBarrier.buffer = dstBuffer.bufferHandle;
+            dstBufferBarrier.offset = dstBuffer.bufferAllocInfo.offset;
+            dstBufferBarrier.size = dstBuffer.bufferAllocInfo.size;
+            dstBufferBarrier.pNext = nullptr;
+
+            requiredBarriers.bufferBarriers.push_back(dstBufferBarrier);
+        }
+        return requiredBarriers;
+    }
 };
 
 struct CopyImageCommand : public CommandRecord
