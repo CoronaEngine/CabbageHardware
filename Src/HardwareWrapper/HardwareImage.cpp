209f83b46bd16cab238cf2627614d8ed88d8aecf
Michael <1216866818@qq.com>
2025-11-06 18:42:03 +0800

    feat: 重新修改完HardwareImage

--- Src/HardwareWrapper/HardwareImage.cpp
+++ Src/HardwareWrapper/HardwareImage.cpp
@@ -4,162 +4,10 @@
 
 Corona::Kernel::Utils::Storage<ResourceManager::ImageHardwareWrap> globalImageStorages;
 
-HardwareImage& HardwareImage::operator=(const HardwareImage& other)
-{
-    auto otherHandle = static_cast<uintptr_t>(*other.imageIdPtr);
-    auto thisHandle = static_cast<uintptr_t>(*this->imageIdPtr);
-
-    if (otherHandle != thisHandle)
-    {
-        bool write_success = globalImageStorages.write(otherHandle, [](ResourceManager::ImageHardwareWrap &image) {
-            image.refCount++;
-        });
-
-        if (!write_success)
-        {
-            throw std::runtime_error("Failed to write HardwareBuffer!");
-        }
-    }
-    else
-    {
-        bool write_success = globalImageStorages.write(thisHandle, [&](ResourceManager::ImageHardwareWrap &image) {
-            image.refCount--;
-            if (image.refCount == 0)
-            {
-                globalHardwareContext.mainDevice->resourceManager.destroyImage(image);
-                globalImageStorages.deallocate(thisHandle);
-            }
-        });
-
-        if (!write_success)
-        {
-            throw std::runtime_error("Failed to write HardwareBuffer!");
-        }
-    }
-
-    *(this->imageIdPtr) = *(other.imageIdPtr);
-    return *this;
-}
-
 HardwareImage::HardwareImage()
 {
-    this->imageIdPtr = std::make_shared<uint64_t>(std::numeric_limits<uint64_t>::max());
+    this->imageID = std::make_shared<uintptr_t>(std::numeric_limits<uintptr_t>::max());
 }
-
-HardwareImage::HardwareImage(const HardwareImage &other)
-{
-    this->imageIdPtr = other.imageIdPtr;
-
-    auto otherHandle = static_cast<uintptr_t>(*other.imageIdPtr);
-    bool write_success = globalImageStorages.write(otherHandle, [](ResourceManager::ImageHardwareWrap &image) {
-        image.refCount++;
-    });
-
-    if (!write_success)
-    {
-        throw std::runtime_error("Failed to write HardwareBuffer!");
-    }
-}
-
-HardwareImage::~HardwareImage()
-{
-    auto thisHandle = static_cast<uintptr_t>(*this->imageIdPtr);
-    bool write_success = globalImageStorages.write(thisHandle, [&](ResourceManager::ImageHardwareWrap &image) {
-        image.refCount--;
-        if (image.refCount == 0)
-        {
-            globalHardwareContext.mainDevice->resourceManager.destroyImage(image);
-            globalImageStorages.deallocate(thisHandle);
-        }
-    });
-
-    if (!write_success)
-    {
-        throw std::runtime_error("Failed to write HardwareBuffer!");
-    }
-}
-
-HardwareImage::operator bool()
-{
-    if (imageIdPtr != nullptr)
-    {
-        bool read_success = globalImageStorages.read(static_cast<uintptr_t>(*imageIdPtr), [](const ResourceManager::ImageHardwareWrap &image) {
-            return image.imageHandle != VK_NULL_HANDLE;
-        });
-
-        if (read_success)
-        {
-            throw std::runtime_error("Failed to read HardwareBuffer!");
-        }
-    }
-    else
-    {
-        return false;
-    }
-}
-
-uint32_t HardwareImage::storeDescriptor()
-{
-    ResourceManager::ImageHardwareWrap storeImage;
-    bool read_success = globalImageStorages.read(static_cast<uintptr_t>(*imageIdPtr), [&](const ResourceManager::ImageHardwareWrap &image) {
-        storeImage = image;
-    });
-
-    return globalHardwareContext.mainDevice->resourceManager.storeDescriptor(storeImage);
-}
-
-HardwareImage &HardwareImage::copyFromBuffer(const HardwareBuffer &buffer)
-{
-    HardwareExecutor tempExecutor;
-
-    ResourceManager::BufferHardwareWrap srcBuffer;
-    ResourceManager::ImageHardwareWrap dstImage;
-
-    bool read_srcBuffer_success = globalBufferStorages.read(static_cast<uintptr_t>(*buffer.bufferIdPtr), [&](const ResourceManager::BufferHardwareWrap &buffer) {
-        srcBuffer = buffer;
-    });
-    bool read_dstImage_success = globalImageStorages.read(static_cast<uintptr_t>(*imageIdPtr), [&](const ResourceManager::ImageHardwareWrap &image) {
-        dstImage = image;
-    });
-
-    // 使用栅栏确保提交完成后再返回，避免临时 staging buffer 在 GPU 仍然读取时被析构
-    CopyBufferToImageCommand copyCmd(srcBuffer, dstImage);
-
-    //VkFenceCreateInfo fenceInfo{};
-    //fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
-    //fenceInfo.flags = 0; // 未信号状态
-
-    //VkDevice device = globalHardwareContext.mainDevice->deviceManager.logicalDevice;
-    //VkFence fence = VK_NULL_HANDLE;
-    //if (vkCreateFence(device, &fenceInfo, nullptr, &fence) != VK_SUCCESS)
-    //{
-    //    // 创建失败则退化为同步 DeviceIdle，保证安全性
-    //    tempExecutor << &copyCmd << tempExecutor.commit();
-    //    vkDeviceWaitIdle(device);
-    //    return *this;
-    //}
-
-    tempExecutor << &copyCmd << tempExecutor.commit();
-
-    // 等待 GPU 完成该次提交，确保源 buffer 生命周期安全
-    //vkWaitForFences(device, 1, &fence, VK_TRUE, UINT64_MAX);
-    //vkDestroyFence(device, fence, nullptr);
-
-    return *this;
-}
-
-HardwareImage &HardwareImage::copyFromData(const void *inputData)
-{
-    ResourceManager::ImageHardwareWrap copyImage;
-    bool read_success = globalImageStorages.read(static_cast<uintptr_t>(*imageIdPtr), [&](const ResourceManager::ImageHardwareWrap &image) {
-        copyImage = image;
-    });
-
-    HardwareBuffer stagingBuffer = HardwareBuffer(copyImage.imageSize.x * copyImage.imageSize.y * copyImage.pixelSize, BufferUsage::StorageBuffer, inputData);
-    copyFromBuffer(stagingBuffer);
-    return *this;
-}
-
 
 HardwareImage::HardwareImage(uint32_t width, uint32_t height, ImageFormat imageFormat, ImageUsage imageUsage, int arrayLayers, void *imageData)
 {
@@ -244,4 +92,115 @@ HardwareImage::HardwareImage(uint32_t width, uint32_t height, ImageFormat imageF
             copyFromData(imageData);
         }
     });
+
+    this->imageID = std::make_shared<uintptr_t>(handle);
+}
+
+HardwareImage::HardwareImage(const HardwareImage &other)
+{
+    this->imageID = other.imageID;
+
+    globalImageStorages.write(*other.imageID, [](ResourceManager::ImageHardwareWrap &image) {
+        image.refCount++;
+    });
+}
+
+HardwareImage::~HardwareImage()
+{
+    globalImageStorages.write(*imageID, [&](ResourceManager::ImageHardwareWrap &image) {
+        image.refCount--;
+        if (image.refCount == 0)
+        {
+            globalHardwareContext.mainDevice->resourceManager.destroyImage(image);
+            globalImageStorages.deallocate(*imageID);
+        }
+    });
+}
+
+HardwareImage& HardwareImage::operator=(const HardwareImage& other)
+{
+    globalImageStorages.write(*other.imageID, [](ResourceManager::ImageHardwareWrap &image) {
+        image.refCount++;
+    });
+
+    globalImageStorages.write(*this->imageID, [&](ResourceManager::ImageHardwareWrap &image) {
+        image.refCount--;
+        if (image.refCount == 0)
+        {
+            globalHardwareContext.mainDevice->resourceManager.destroyImage(image);
+            globalImageStorages.deallocate(*imageID);
+        }
+    });
+
+    *(this->imageID) = *(other.imageID);
+    return *this;
+}
+
+HardwareImage::operator bool()
+{
+    if (imageID != nullptr)
+    {
+        globalImageStorages.read(*imageID, [](const ResourceManager::ImageHardwareWrap &image) {
+            return image.imageHandle != VK_NULL_HANDLE;
+        });
+    }
+
+    return false;
+}
+
+uint32_t HardwareImage::storeDescriptor()
+{
+    globalImageStorages.read(*imageID, [&](const ResourceManager::ImageHardwareWrap &image) {
+        return globalHardwareContext.mainDevice->resourceManager.storeDescriptor(image);
+    });
+}
+
+HardwareImage &HardwareImage::copyFromBuffer(const HardwareBuffer &buffer)
+{
+    HardwareExecutor tempExecutor;
+
+    ResourceManager::BufferHardwareWrap srcBuffer;
+    ResourceManager::ImageHardwareWrap dstImage;
+
+    globalBufferStorages.read(*buffer.bufferID, [&](const ResourceManager::BufferHardwareWrap &buffer) {
+        srcBuffer = buffer;
+    });
+    globalImageStorages.read(*imageID, [&](const ResourceManager::ImageHardwareWrap &image) {
+        dstImage = image;
+    });
+
+    // 使用栅栏确保提交完成后再返回，避免临时 staging buffer 在 GPU 仍然读取时被析构
+    CopyBufferToImageCommand copyCmd(srcBuffer, dstImage);
+
+    //VkFenceCreateInfo fenceInfo{};
+    //fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+    //fenceInfo.flags = 0; // 未信号状态
+
+    //VkDevice device = globalHardwareContext.mainDevice->deviceManager.logicalDevice;
+    //VkFence fence = VK_NULL_HANDLE;
+    //if (vkCreateFence(device, &fenceInfo, nullptr, &fence) != VK_SUCCESS)
+    //{
+    //    // 创建失败则退化为同步 DeviceIdle，保证安全性
+    //    tempExecutor << &copyCmd << tempExecutor.commit();
+    //    vkDeviceWaitIdle(device);
+    //    return *this;
+    //}
+
+    tempExecutor << &copyCmd << tempExecutor.commit();
+
+    // 等待 GPU 完成该次提交，确保源 buffer 生命周期安全
+    //vkWaitForFences(device, 1, &fence, VK_TRUE, UINT64_MAX);
+    //vkDestroyFence(device, fence, nullptr);
+
+    return *this;
+}
+
+HardwareImage &HardwareImage::copyFromData(const void *inputData)
+{
+    globalImageStorages.read(*imageID, [&](const ResourceManager::ImageHardwareWrap &image) {
+        HardwareBuffer stagingBuffer = HardwareBuffer(image.imageSize.x * image.imageSize.y * image.pixelSize, BufferUsage::StorageBuffer, inputData);
+        copyFromBuffer(stagingBuffer);
+    });
+
+    return *this;
 }
