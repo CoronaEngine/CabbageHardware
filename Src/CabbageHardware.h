eeb9dadd17b12e3ae31378fdba518b070bb7f5b9
Michael <1216866818@qq.com>
2025-11-13 14:12:25 +0800

    Update CabbageHardware.h

--- Src/CabbageHardware.h
+++ Src/CabbageHardware.h
@@ -26,7 +26,7 @@ enum class ImageFormat : uint32_t
     RG32_FLOAT,
 
     D16_UNORM,
-    D32_FLOAT, // just for depth
+    D32_FLOAT,
 };
 
 enum class ImageUsage : uint32_t
@@ -36,12 +36,19 @@ enum class ImageUsage : uint32_t
     DepthImage = 3,
 };
 
-enum class BufferUsage : int32_t
+enum class BufferUsage : uint32_t
 {
     VertexBuffer = 1,
-    IndexBuffer = 2, // must be uint32_t
+    IndexBuffer = 2,
     UniformBuffer = 4,
     StorageBuffer = 8,
+};
+
+template <typename T>
+concept IsContainer = requires(T a) {
+    { a.size() } -> std::convertible_to<size_t>;
+    { a.data() };
+    { a[0] };
 };
 
 struct ExternalHandle
@@ -53,103 +60,115 @@ struct ExternalHandle
 #endif
 };
 
-template <typename T>
-concept is_container = requires(T a) {
-    a.size();
-    a.data();
-    a[0];
-};
-
 struct HardwareBuffer
 {
+  public:
     HardwareBuffer();
     HardwareBuffer(const HardwareBuffer &other);
     HardwareBuffer(uint32_t bufferSize, uint32_t elementSize, BufferUsage usage, const void *data = nullptr);
+
     HardwareBuffer(uint32_t size, BufferUsage usage, const void *data = nullptr)
         : HardwareBuffer(1, size, usage, data)
     {
-
     }
 
-    ExternalHandle exportBufferMemory();
-    HardwareBuffer importBufferMemory(const ExternalHandle &memHandle);
-
-    template <typename Type>
-    HardwareBuffer(const Type &input, BufferUsage usage)
-        requires is_container<Type>
+    template <IsContainer Container>
+    HardwareBuffer(const Container &input, BufferUsage usage)
         : HardwareBuffer(input.size(), sizeof(input[0]), usage, input.data())
     {
     }
 
     ~HardwareBuffer();
 
-    uint32_t storeDescriptor();
+    HardwareBuffer &operator=(const HardwareBuffer &other);
+    explicit operator bool() const;
+
+    ExternalHandle exportBufferMemory();
+    HardwareBuffer importBufferMemory(const ExternalHandle &memHandle);
+
+    [[nodiscard]] uint32_t storeDescriptor();
 
     bool copyFromBuffer(const HardwareBuffer &inputBuffer, HardwareExecutor *executor);
+
     bool copyFromData(const void *inputData, uint64_t size);
 
-    template <typename Type>
-    bool copyFromVector(const std::vector<Type> &input)
+    template <typename T>
+    bool copyFromVector(const std::vector<T> &input)
     {
-        copyFromData(input.data(), input.size() * sizeof(Type));
-        return true;
+        return copyFromData(input.data(), input.size() * sizeof(T));
     }
 
-    void* getMappedData();
+    [[nodiscard]] void *getMappedData();
 
-    uint64_t getBufferSize();
+    [[nodiscard]] uint64_t getBufferSize() const;
 
-    HardwareBuffer &operator=(const HardwareBuffer &other);
+    [[nodiscard]] std::shared_ptr<uintptr_t> getBufferID() const
+    {
+        return bufferID;
+    }
 
-    operator bool();
-
+  private:
     std::shared_ptr<uintptr_t> bufferID;
+
+    friend class HardwareImage;
 };
 
 struct HardwareImage
 {
+  public:
     HardwareImage();
     HardwareImage(const HardwareImage &other);
     HardwareImage(uint32_t width, uint32_t height, ImageFormat imageFormat, ImageUsage imageUsage = ImageUsage::SampledImage, int arrayLayers = 1, void *imageData = nullptr);
 
     ~HardwareImage();
 
-    operator bool();
-
-    uint32_t storeDescriptor();
-
-    //bool blitFromImage(const HardwareImage &buffer);
-    //bool copyFromImage(const HardwareImage &buffer);
-
     HardwareImage &operator=(const HardwareImage &other);
+    explicit operator bool() const;
 
-    std::shared_ptr<uintptr_t> imageID;
+    [[nodiscard]] uint32_t storeDescriptor();
+
+    [[nodiscard]] std::shared_ptr<uintptr_t> getImageID() const
+    {
+        return imageID;
+    }
 
   private:
     HardwareImage &copyFromBuffer(const HardwareBuffer &buffer);
+
     HardwareImage &copyFromData(const void *inputData);
+
+    std::shared_ptr<uintptr_t> imageID;
+
+    friend class HardwareDisplayer;
 };
 
 struct HardwarePushConstant
 {
+  public:
     HardwarePushConstant();
-    ~HardwarePushConstant();
-
     HardwarePushConstant(const HardwarePushConstant &other);
 
-    template <typename Type>
-    HardwarePushConstant(Type data)
-        requires(!std::is_same_v<std::remove_cvref_t<Type>, HardwarePushConstant>)
+    template <typename T>
+    HardwarePushConstant(T data)
+        requires(!std::is_same_v<std::remove_cvref_t<T>, HardwarePushConstant>)
     {
-        copyFromRaw(&data, sizeof(Type));
+        copyFromRaw(&data, sizeof(T));
     }
 
     HardwarePushConstant(uint64_t size, uint64_t offset, HardwarePushConstant *whole = nullptr);
 
+    ~HardwarePushConstant();
+
     HardwarePushConstant &operator=(const HardwarePushConstant &other);
 
-    uint8_t *getData() const;
-    uint64_t getSize() const;
+    [[nodiscard]] uint8_t *getData() const;
+
+    [[nodiscard]] uint64_t getSize() const;
+
+    [[nodiscard]] std::shared_ptr<uintptr_t> getPushConstantID() const
+    {
+        return pushConstantID;
+    }
 
   private:
     void copyFromRaw(const void *src, uint64_t size);
@@ -159,15 +178,21 @@ struct HardwarePushConstant
 
 struct HardwareDisplayer
 {
-    HardwareDisplayer(void *surface = nullptr);
+  public:
+    explicit HardwareDisplayer(void *surface = nullptr);
     HardwareDisplayer(const HardwareDisplayer &other);
     ~HardwareDisplayer();
 
     HardwareDisplayer &operator=(const HardwareDisplayer &other);
-
     HardwareDisplayer &operator<<(HardwareImage &image);
 
     HardwareDisplayer &wait(HardwareExecutor &executor);
 
+    [[nodiscard]] std::shared_ptr<uintptr_t> getDisplayerID() const
+    {
+        return displaySurfaceID;
+    }
+
+  private:
     std::shared_ptr<uintptr_t> displaySurfaceID;
 };
