5397e47f10fec6dcb445d3be6c235c1d6e7ac352
Michael <1216866818@qq.com>
2025-11-13 14:20:24 +0800

    Update ComputePipeline.h

--- Src/Pipeline/ComputePipeline.h
+++ Src/Pipeline/ComputePipeline.h
@@ -2,39 +2,24 @@
 
 #include <ktm/ktm.h>
 
-#include"../Hardware/DeviceManager.h"
-#include"../Hardware/ResourceManager.h"
+#include "../Hardware/DeviceManager.h"
+#include "../Hardware/ResourceManager.h"
 
-#include"Compiler/ShaderCodeCompiler.h"
+#include "Compiler/ShaderCodeCompiler.h"
 
-#include"../CabbageHardware.h"
-
+#include "../CabbageHardware.h"
 
 struct ComputePipeline : public CommandRecord
 {
-    ComputePipeline()
-    {
-        executorType = CommandRecord::ExecutorType::Compute;
-    }
-
-    ~ComputePipeline();
+  public:
+    ComputePipeline();
+    ~ComputePipeline() override;
 
     ComputePipeline(std::string shaderCode, EmbeddedShader::ShaderLanguage language = EmbeddedShader::ShaderLanguage::GLSL, const std::source_location &sourceLocation = std::source_location::current());
 
-    std::variant<HardwarePushConstant> operator[](const std::string& resourceName)
-    {
-        EmbeddedShader::ShaderCodeModule::ShaderResources::ShaderBindInfo *resource = shaderResource.findShaderBindInfo(resourceName);
-        if (resource != nullptr && resource->bindType == EmbeddedShader::ShaderCodeModule::ShaderResources::BindType::pushConstantMembers)
-        {
-            return std::move(HardwarePushConstant(resource->typeSize, resource->byteOffset, &pushConstant));
-        }
-        else
-        {
-            throw std::runtime_error("failed to find with name!" + resourceName);
-        }
-    }
+    std::variant<HardwarePushConstant> operator[](const std::string &resourceName);
 
-    ComputePipeline* operator()(uint16_t x, uint16_t y, uint16_t z);
+    ComputePipeline *operator()(uint16_t x, uint16_t y, uint16_t z);
 
     ExecutorType getExecutorType() override
     {
@@ -42,19 +27,16 @@ struct ComputePipeline : public CommandRecord
     }
 
     void commitCommand(HardwareExecutor &hardwareExecutor) override;
-
     RequiredBarriers getRequiredBarriers(HardwareExecutor &hardwareExecutor) override;
 
   private:
+    void createComputePipeline();
 
-	VkPipelineLayout pipelineLayout = VK_NULL_HANDLE;
-	VkPipeline pipeline = VK_NULL_HANDLE;
+    VkPipelineLayout pipelineLayout = VK_NULL_HANDLE;
+    VkPipeline pipeline = VK_NULL_HANDLE;
 
-	HardwarePushConstant pushConstant;
-    //HardwarePushConstant tempPushConstantMember;
+    HardwarePushConstant pushConstant;
     EmbeddedShader::ShaderCodeModule::ShaderResources shaderResource;
-
-    //EmbeddedShader::ShaderCodeCompiler shaderCodeCompiler;
     EmbeddedShader::ShaderCodeModule shaderCode;
 
     ktm::uvec3 groupCount = {0, 0, 0};
