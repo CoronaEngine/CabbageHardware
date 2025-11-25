089255860afabad7f6601745a7ba7a9acce897ed
与湫风起 <60731523+lh472266503@users.noreply.github.com>
2025-11-25 20:34:33 +0800

    fixed: 暂时注释掉display thread

--- Examples/main.cpp
+++ Examples/main.cpp
@@ -140,7 +140,7 @@ int main() {
     // testCompressedTextures();
     auto now = std::chrono::system_clock::now();
     std::time_t now_c = std::chrono::system_clock::to_time_t(now);
-    
+
     std::tm now_tm;
     localtime_s(&now_tm, &now_c);
     char time_buffer[64];
@@ -154,7 +154,7 @@ int main() {
         Corona::Kernel::CoronaLogger::info("Main thread started...");
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 
-        std::vector<GLFWwindow*> windows(2);
+        std::vector<GLFWwindow*> windows(6);
         for (size_t i = 0; i < windows.size(); i++) {
             windows[i] = glfwCreateWindow(1920, 1080, "Cabbage Engine ", nullptr, nullptr);
         }
@@ -268,18 +268,18 @@ int main() {
             }
         };
 
-        auto displayThread = [&](uint32_t threadIndex) {
-            Corona::Kernel::CoronaLogger::info("Display thread started...");
-            HardwareDisplayer displayManager = HardwareDisplayer(glfwGetWin32Window(windows[threadIndex]));
-            while (running.load()) {
-                displayManager.wait(executors[threadIndex]) << finalOutputImages[threadIndex];
-            }
-        };
+        // auto displayThread = [&](uint32_t threadIndex) {
+        //     Corona::Kernel::CoronaLogger::info("Display thread started...");
+        //     HardwareDisplayer displayManager = HardwareDisplayer(glfwGetWin32Window(windows[threadIndex]));
+        //     while (running.load()) {
+        //         displayManager.wait(executors[threadIndex]) << finalOutputImages[threadIndex];
+        //     }
+        // };
 
         for (size_t i = 0; i < windows.size(); i++) {
             std::thread(meshThread, i).detach();
             std::thread(renderThread, i).detach();
-            std::thread(displayThread, i).detach();
+            // std::thread(displayThread, i).detach();
         }
 
         while (running.load()) {
