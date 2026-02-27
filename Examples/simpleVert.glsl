1aa396dc7e4d4f916419e9b9b074bb56f67b8ed6
Michael <1216866818@qq.com>
2026-02-27 22:50:45 +0800

    fix: 继续简化顶点着色器

--- Examples/simpleVert.glsl
+++ Examples/simpleVert.glsl
@@ -1,36 +1,27 @@
 #version 450
 #extension GL_EXT_nonuniform_qualifier : enable
 
-layout(set = 1, binding = 0) readonly buffer StorageBufferObject
+layout(location = 0) in vec3 inPosition;  
+//layout(location = 1) in vec3 inColor;    
+layout(location = 1) in vec2 inUV;
+
+
+//layout(location = 0) out vec3 outColor;   
+layout(location = 1) out vec2 outUV;  
+
+layout(binding = 0) uniform VPMatrices
 {
-    uint textureIndex;
-    mat4 model;
-    mat4 view;
-    mat4 proj;
-    vec3 viewPos;
-    vec3 lightColor;
-    vec3 lightPos;
-} storageBufferObject;
+    mat4 mViewMatrix;                     
+    mat4 mProjectionMatrix;              
+}vpUBO;
 
-layout(location = 0) in vec3 inPosition;
-layout(location = 1) in vec3 inNormal;
-layout(location = 2) in vec2 inTexCoord;
-layout(location = 3) in vec3 inColor;
-
-layout(location = 0) out vec3 fragPos;
-layout(location = 1) out vec3 fragNormal;
-layout(location = 2) out vec2 fragTexCoord;
-layout(location = 3) out vec3 fragColor;
+layout(binding = 1) uniform ObjectUniform
+{
+    mat4 mModelMatrix;               
+}objectUBO;
 
 void main()
 {
-    gl_Position = storageBufferObject.proj * storageBufferObject.view * storageBufferObject.model * vec4(inPosition, 1.0);           
-
-    fragPos = vec3(storageBufferObject.model * vec4(inPosition, 1.0));
-    
-    // 去掉了 inverse 和 transpose，直接使用 model 矩阵来变换法线
-    fragNormal = normalize(mat3(storageBufferObject.model) * inNormal);
-    
-    fragColor = inColor;
-    fragTexCoord = inTexCoord;
+    gl_Position = vpUBO.mProjectionMatrix * vpUBO.mViewMatrix * objectUBO.mModelMatrix * vec4(inPosition, 1.0);
+    outUV = inUV;        
 }
