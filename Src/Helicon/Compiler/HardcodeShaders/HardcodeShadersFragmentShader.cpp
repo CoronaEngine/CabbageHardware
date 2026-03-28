df23dcf6a205fd8372f941978ad429c9a23e2577
GraphZou <906520989@qq.com>
2026-03-28 21:25:07 +0800

    fix HardcodeShaders文件夹

---
+++ Src/Helicon/Compiler/HardcodeShaders/HardcodeShadersFragmentShader.cpp
@@ -0,0 +1,2 @@
+#include"HardcodeShaders.h"
+std::unordered_map<std::string, std::variant<EmbeddedShader::ShaderCodeModule::ShaderResources,std::variant<std::vector<uint32_t>,std::string>>> EmbeddedShader::HardcodeShaders::hardcodeShadersFragmentShader = {};
